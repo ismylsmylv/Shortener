146f30a929b5a11c5d06b48ff7c850232aa01675
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-09-01 18:07:54 +0400

    feat: input form added

---
+++ src/components/input/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { useState } from "react";
+import "./style.scss";
+type Props = {};
+
+function InputView({}: Props) {
+  const [url, seturl] = useState("");
+  function getURL() {
+    console.log(url);
+    seturl("");
+  }
+  return (
+    <div className="InputView flex justify-center items-center flex-col	">
+      <div className="logo">shortener</div>
+      <form
+        className="input flex justify-center items-center gap-5 rounded"
+        action={getURL}
+      >
+        <input
+          type="url"
+          placeholder="Enter the link here"
+          className="outline-none"
+          required
+          value={url}
+          onChange={(e) => {
+            seturl(e.target.value);
+          }}
+        />
+        <button type="submit">get short</button>
+      </form>
+    </div>
+  );
+}
+
+export default InputView;
---
+++ src/view/page.tsx
@@ -0,0 +1,10 @@
+import InputView from "@/components/input/page";
+import React from "react";
+
+type Props = {};
+
+function HomeView({}: Props) {
+  return <InputView />;
+}
+
+export default HomeView;
