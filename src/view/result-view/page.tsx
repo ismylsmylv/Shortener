11996f878f79f4b97299b60b19e6331ef394c5c7
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-09-01 22:59:09 +0400

    feat: retrieving long url form db added

--- src/view/result-view/page.tsx
+++ src/view/result-view/page.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 type Props = {
   params: { url: string };
 };
 
 function ResultView({ params }: Props) {
+  const [url, seturl] = useState();
+  useEffect(() => {
+    axios
+      .get(process.env.NEXT_PUBLIC_BASE_URI + "/api?id=" + params.url)
+      .then((res) => {
+        seturl(res.data.long);
+      });
+    console.log(process.env.NEXT_PUBLIC_BASE_URI + "/api?id=" + params.url);
+  }, []);
   return (
     <div>
       <h1>Dynamic ID: {params.url}</h1>
+      <p>result {url}</p>
     </div>
   );
 }
